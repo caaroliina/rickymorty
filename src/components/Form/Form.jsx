9ef2541cadf43069b487da22296b30e5cfe4adcd
caaroliina <carogrzn97@gmail.com>
2023-04-25 23:33:52 -0300

    Changes to be committed: 	modified:   src/components/Form/Form.jsx

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -8,7 +8,7 @@ function Form (){
         password: "",
     });
 
-    const [errors, setErrors] = useState({});
+    // const [errors, setErrors] = useState({});
 
     const handleChange = (event) => {
         setUserData({
