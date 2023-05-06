f21c127566ce1a5279ab9432ec9cc340c7586683
Carolina Garzon <carogrzn97@gmail.com>
2023-05-06 18:22:13 -0300

    Changes to be committed: 	modified:   Client/package-lock.json 	modified:   Client/package.json 	modified:   Server/src/index.js

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,12 +1,12 @@
 const http = require("http");
-const { getCharById } = require("./controllers/getCharById.js")
+const { getCharById } = require("./controllers/getCharById.js");
 const PORT = 3001;
 
 http.createServer((req, res)=>{
     res.setHeader('Access-Control-Allow-Origin', '*');
     if(req.url.includes('/rickandmorty/character')){
         let id = req.url.split("/").at(-1);
-        getCharById(res, +id)
+        getCharById(res, +id);
     }
 })
 .listen(PORT, 'localhost');
