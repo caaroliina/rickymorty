817866cdb4a65a80d29b83ee73f726d3ae5e978b
Carolina Garzon <carogrzn97@gmail.com>
2023-05-07 21:15:29 -0300

    Changes to be committed: 	modified:   Client/src/components/Favorites/Favorites.jsx 	modified:   Client/src/redux/action-type.js 	modified:   Client/src/redux/action.js 	modified:   Client/src/redux/reducer.js

--- Client/src/components/Favorites/Favorites.jsx
+++ Client/src/components/Favorites/Favorites.jsx
@@ -1,9 +1,34 @@
-import { connect } from "react-redux"
-import Card from "../Card/Card"
+import { connect, useDispatch } from "react-redux";
+import { useState } from "react";
+import { orderCards, filterCards } from "../../redux/action"
+import Card from "../Card/Card";
 
 const Favorites = ({myFavorites})=> {
+
+    const dispatch = useDispatch();
+
+    const [aux, setAux] = useState(false);
+
+    const handleOrder = (event) => {
+        dispatch(orderCards(event.target.value));
+        setAux(true);
+    }
+
+    const handleFilter = (event) => dispatch(filterCards(event.target.value))
+
     return(
         <div>
+            <select onChange={handleOrder}>
+                <option value="A" >Ascendente</option>
+                <option value="D" >Descendente</option>
+            </select>
+            <select onChange={handleFilter}>
+                <option value="Male" >Hombre</option>
+                <option value="Female" >Mujer</option>
+                <option value="Genderless">Sin Género</option>
+                <option value="unknown" >Desconocido</option>
+                <option value="allCharacters"> Mostrar todos </option>
+            </select>
             {
                 myFavorites?.map(fav => {
                     return (
--- Client/src/redux/reducer.js
+++ Client/src/redux/reducer.js
@@ -1,8 +1,8 @@
-import { ADD_FAV, REMOVE_FAV } from "./action-type"
+import { ADD_FAV, FILTER, ORDER, REMOVE_FAV } from "./action-type"
 
 const initialState = {
     myFavorites: [],
-
+    allCharacters: [],
 }
 
 const reducer = (state = initialState, { type, payload })=>{
@@ -10,12 +10,29 @@ const reducer = (state = initialState, { type, payload })=>{
         case ADD_FAV:
             return {
                 ...state,
-                myFavorites: [...state.myFavorites, payload]
+                myFavorites: [...state.allCharacters, payload],
+                allCharacters: [...state.allCharacters, payload],
             }
         case REMOVE_FAV:
             return {
                 ...state,
                 myFavorites: state.myFavorites.filter(character => character.id !== payload)
+            }
+        case FILTER:
+            const allCharactersFilter = state.allCharacters.filter(character => character.gender === payload)
+            return{
+                ...state,
+                myFavorites: payload === 'allCharacters'
+                ? [...state.allCharacters]
+                : allCharactersFilter,
+            }
+        case ORDER:
+            const allfav = [...state.allCharacters]
+            return {
+                ...state,
+                myFavorites:  payload === 'A' 
+                ? allfavsort((a, b)=> a.id - b.id) 
+                : allfav.sort((a, b)=> b.id - a.id),
             }
         default:
             return {...state}
